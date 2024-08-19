084d8bbc561f9867c188260f5f3b463fe4884c18
Oltda <dan.olt@hotmail.cz>
2024-08-19 23:04:47 +0200

    same site none

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -60,7 +60,7 @@ router.post(
             res.cookie("token", token, {
               httpOnly: true,
               secure: false,
-              sameSite: "Lax",
+              sameSite: "None",
               maxAge: 3600000,
             })
             // return res.json("Success")
